49b1bb6b400434cef6c1497af78ff44e1240a94c
Christian Prado <christianprado.contato@gmail.com>
2020-03-28 00:21:48 -0300

    Done mobile app

---
+++ mobile/src/interfaces/index.d.ts
@@ -0,0 +1,13 @@
+export interface IncidentProps {
+  id: number;
+  title: string;
+  description: string;
+  value: number;
+  ongId: string;
+  name: string;
+  email: string;
+  whatsapp: string;
+  city: string;
+  uf: string;
+}
+
